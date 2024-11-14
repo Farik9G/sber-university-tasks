4ffd178063e562a6f5ed41239e20f4e4b620603d
Farik9G <92394604+Farik9G@users.noreply.github.com>
2024-11-14 22:30:21 +0300

    Add task1 solution

---
+++ task1/src/main/java/ru/meeral/Main.java
@@ -0,0 +1,10 @@
+package ru.meeral;
+
+import ru.meeral.classes.Child;
+
+public class Main {
+    public static void main(String[] args) {
+        Child child = new Child();
+        Child childIvan = new Child("Ivan");
+    }
+}
